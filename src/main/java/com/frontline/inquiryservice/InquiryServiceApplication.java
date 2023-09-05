ecd8a7cf23100972335d0032fc0d0bfdbba1abe1
kuldeep14691 <kuldeep3727@gmail.com>
2023-09-05 23:16:19 +0530

    first commit

---
+++ src/main/java/com/frontline/inquiryservice/InquiryServiceApplication.java
@@ -0,0 +1,22 @@
+package com.frontline.inquiryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class InquiryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InquiryServiceApplication.class, args);
+	}
+}
+@RestController
+class MyController {
+
+	@GetMapping("/frontline")
+	public String hello() {
+		return "Hello Frontline";
+	}
+}
